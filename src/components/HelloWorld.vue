cb8f2eb97f8e9eba291b3304d4a165b4c991b838
Savan-ITDEV <savanitdev@gmail.com>
2019-09-09 17:09:26 +0700

    bk1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <v-btn @click="alertDisplay">Hello world</v-btn>
+        <b-button  @click="alertDisplay" variant="primary">Primary</b-button>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+         text: '',
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+    },
+     methods: {
+      alertDisplay() {
+        // $swal function calls SweetAlert into the application with the specified configuration.
+        this.$swal('Heading', 'this is a Heading', 'OK');
+      }
+    }
+  }
+</script>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import Vue from 'vue';
+import './plugins/bootstrap-vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import VueSweetalert2 from 'vue-sweetalert2';
+Vue.config.productionTip = false;
+Vue.use(VueSweetalert2);
+new Vue({
+	router,
+	store,
+  vuetify,
+	render: (h) => h(App)
+}).$mount('#app');
